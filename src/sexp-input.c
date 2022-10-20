1aea9165a69fd2a8b1f311ada2e8679e03db6980
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2022-10-20 10:08:51 +0300

    Removed deprecated malloc.h

--- src/sexp-input.c
+++ src/sexp-input.c
@@ -4,7 +4,6 @@
  */
 
 #include <stdio.h>
-#include <malloc.h>
 #include "sexp.h"
 
 /**************************************/
--- src/sexp-main.c
+++ src/sexp-main.c
@@ -4,7 +4,6 @@
  */
 
 #include <stdio.h>
-#include <malloc.h>
 #include <stdlib.h>
 #include "sexp.h"
 
