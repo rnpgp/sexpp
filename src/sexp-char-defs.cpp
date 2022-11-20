b981d11655c116b11cd120d3910115b4ecd81d86
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2022-11-20 22:19:09 +0300

    Added offline Gtest and sanitizers GHA job

--- src/sexp-char-defs.cpp
+++ src/sexp-char-defs.cpp
@@ -47,42 +47,52 @@ unsigned char sexp_char_defs_t::hexvalue[256];    /* a value of c as a hex digit
 bool          sexp_char_defs_t::base64digit[256]; /* c is base64 digit */
 unsigned char sexp_char_defs_t::base64value[256]; /* a value of c as base64 digit */
 bool          sexp_char_defs_t::tokenchar[256];   /* c can be in a token */
-
+bool          sexp_char_defs_t::alpha[256];       /* c is alphabetic A-Z a-z */
+bool          sexp_char_defs_t::whitespace[256];  /* c is whitespace */
+bool          sexp_char_defs_t::decdigit[256];    /* c is a dec digit */
+bool          sexp_char_defs_t::hexdigit[256];    /* c is a hex digit */
 /*
  * sexp_char_defs_t::initializeCharacterTables
  * initializes all of the above arrays
  */
-void sexp_char_defs_t::initialize_character_tables()
+
+void sexp_char_defs_t::initialize_character_tables(void)
 {
     int i;
     for (i = 0; i < 256; i++) {
         upper[i] = i;
-        base64digit[i] = false;
-        tokenchar[i] = false;
-    }
-    for (i = 'a'; i <= 'z'; i++)
-        upper[i] = i - 'a' + 'A';
-    for (i = '0'; i <= '9'; i++) {
-        base64digit[i] = true;
-        decvalue[i] = hexvalue[i] = i - '0';
-        base64value[i] = (i - '0') + 52;
-    }
-    for (i = 'a'; i <= 'f'; i++) {
-        hexvalue[i] = hexvalue[upper[i]] = i - 'a' + 10;
+        alpha[i] = base64digit[i] = decdigit[i] = hexdigit[i] = whitespace[i] = tokenchar[i] =
+          false;
     }
     for (i = 'a'; i <= 'z'; i++) {
+        upper[i] = i - 'a' + 'A';
         base64digit[i] = base64digit[upper[i]] = true;
+        alpha[i] = alpha[upper[i]] = true;
         base64value[i] = i - 'a' + 26;
         base64value[upper[i]] = i - 'a';
+        tokenchar[i] = tokenchar[upper[i]] = true;
     }
+
+    for (i = '0'; i <= '9'; i++) {
+        base64digit[i] = decdigit[i] = hexdigit[i] = true;
+        decvalue[i] = hexvalue[i] = i - '0';
+        base64value[i] = (i - '0') + 52;
+        tokenchar[i] = true;
+    }
+
+    for (i = 'a'; i <= 'f'; i++) {
+        hexdigit[i] = hexdigit[upper[i]] = true;
+        hexvalue[i] = hexvalue[upper[i]] = i - 'a' + 10;
+    }
+
+    whitespace[' '] = whitespace['\n'] = whitespace['\t'] = true;
+    whitespace['\v'] = whitespace['\r'] = whitespace['\f'] = true;
+
     base64digit['+'] = base64digit['/'] = true;
     base64value['+'] = 62;
     base64value['/'] = 63;
     base64value['='] = 0;
-    for (i = 'a'; i <= 'z'; i++)
-        tokenchar[i] = tokenchar[upper[i]] = true;
-    for (i = '0'; i <= '9'; i++)
-        tokenchar[i] = true;
+
     tokenchar['-'] = true;
     tokenchar['.'] = true;
     tokenchar['/'] = true;
@@ -94,30 +104,30 @@ void sexp_char_defs_t::initialize_character_tables()
 }
 
 /*
- * sexp_char_defs_t::is_white_space(c)
- * Returns true if c is a whitespace character (space, tab, etc. ).
+ * sexp_char_defs::is_white_space(c)
+ * Returns TRUE if c is a whitespace character (space, tab, etc. ).
  */
 bool sexp_char_defs_t::is_white_space(int c)
 {
-    return ((c >= 0 && c <= 255) && std::isspace(c));
+    return ((c >= 0 && c <= 255) && whitespace[c]);
 }
 
 /*
- * sexp_char_defs_t::is_hex_digit(c)
- * Returns true if c is a hexadecimal digit.
- */
-bool sexp_char_defs_t::is_hex_digit(int c)
-{
-    return ((c >= 0 && c <= 255) && std::isxdigit(c));
-}
-
-/*
- * sexp_char_defs_t::is_dec_digit(c)
- * Returns true if c is a decimal digit.
+ * sexp_char_defs::is_dec_digit(c)
+ * Returns TRUE if c is a decimal digit.
  */
 bool sexp_char_defs_t::is_dec_digit(int c)
 {
-    return ((c >= 0 && c <= 255) && std::isdigit(c));
+    return ((c >= 0 && c <= 255) && decdigit[c]);
+}
+
+/*
+ * sexp_char_defs::is_hex_digit(c)
+ * Returns TRUE if c is a hexadecimal digit.
+ */
+bool sexp_char_defs_t::is_hex_digit(int c)
+{
+    return ((c >= 0 && c <= 255) && hexdigit[c]);
 }
 
 /*
@@ -144,7 +154,6 @@ bool sexp_char_defs_t::is_token_char(int c)
  */
 bool sexp_char_defs_t::is_alpha(int c)
 {
-    return ((c >= 0 && c <= 255) && std::isalpha(c));
+    return ((c >= 0 && c <= 255) && alpha[c]);
 }
-
 } // namespace sexp
