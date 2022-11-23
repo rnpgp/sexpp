ca3e8ce1c80b97ab39f9ab7ed13d5433de9225ac
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2022-11-23 17:03:40 +0300

    Implemented sexp-cli tests

--- tests/scripts/tests.sh
+++ tests/scripts/tests.sh
@@ -43,13 +43,6 @@ assert_installed_var() {
 test_install_script() {
    echo "==> Install script test"
 
-   DIR_INS_B="$DIR_INSTALL/bin"
-   DIR_INS_L="$DIR_INSTALL/lib"
-   DIR_INS_L64="$DIR_INSTALL/lib64"
-   DIR_INS_P="$DIR_INS_L/pkgconfig"
-   DIR_INS_P64="$DIR_INS_L64/pkgconfig"
-   DIR_INS_I="$DIR_INSTALL/include/sexp"
-
    if [[ "$OSTYPE" == "windows" ]]; then
       assert_installed "$DIR_INS_B/sexp.exe"
       assert_installed "$DIR_INS_L/sexp.lib"
@@ -63,22 +56,87 @@ test_install_script() {
    assert_installed "$DIR_INS_I/sexp-error.h"
 }
 
+# ......................................................................
+# Check sexp client application
+# THese are the examples from README.adoc
 test_sexp_cli() {
+   echo "==> SEXP client application test"
+
+# On Windows there will be CRLF vs LS mismatch
+# We would rather skip these tests
+   if [[ "$OSTYPE" == "windows" || "$OSTYPE" == "msys" ]]; then
+      startSkipping
+   fi
+
+   app="$DIR_INS_B/sexp"
 # shellcheck disable=SC2251
 ! IFS= read -r -d '' expected << EOM
 Input:
 
 Writing base64 (of canonical) output to certificate.dat
 EOM
-   output=`./sexp -o certificate.dat -p -b <<<"(aa bb (cc dd))"`
-   assertEqual "$expected" "$output"
+   echo "(aa bb (cc dd))" > input1.dat
+   "$app" -o certificate.dat -p -b < input1.dat
+   output=$("$app" -o certificate.dat -p -b < input.dat)
+#  $expected possibly includes extra EOL at the end -- it depends on OS
+   assertContains "$expected" "$output"
+   output=$(cat certificate.dat)
+   assertEquals "{KDI6YWEyOmJiKDI6Y2MyOmRkKSk=}" "$output"
+
+   output=$("$app" -i certificate.dat -x)
+   assertEquals "(2:aa2:bb(2:cc2:dd))" "$output"
+
+# shellcheck disable=SC2251
+! IFS= read -r -d '' expected << EOM
+Reading input from certificate.dat
+
+Canonical output:
+(2:aa2:bb(2:cc2:dd))
+Base64 (of canonical) output:
+{KDI6YWEyOmJiKDI6Y2MyOmRkKSk=}
+Advanced transport output:
+(aa bb (cc dd))
+EOM
+
+   output=$("$app" -i certificate.dat -a -b -c -p -w 0)
+   assertContains "$expected" "$output"
+
+# shellcheck disable=SC2251
+! IFS= read -r -d '' expected << EOM
+Input:
+
+Canonical output:
+(3:abc3:def(3:ghi3:jkl))
+Base64 (of canonical) output:
+{KDM6YWJjMzpkZWYoMzpnaGkzOmprbCkp}
+Advanced transport output:
+(abc def (ghi jkl))
+
+Input:
+EOM
+   echo "(abc def (ghi jkl))" > input2.dat
+   output=$("$app" < input2.dat)
+   assertContains "$expected" "$output"
+
+   if [[ "$OSTYPE" == "windows" || "$OSTYPE" == "msys" ]]; then
+      endSkipping
+   fi
+
 }
+
 # ......................................................................
 # main
-DIR0="$( cd $( dirname $0 ) && pwd )"
+DIR0="$( cd "$( dirname "$0" )" && pwd )"
 DIR1="${DIR_ROOT:="$DIR0/../.."}"
 DIR_ROOT="$( cd "$DIR1" && pwd )"
+
 DIR_INSTALL="$DIR_ROOT/install"
+DIR_INS_B="$DIR_INSTALL/bin"
+DIR_INS_L="$DIR_INSTALL/lib"
+DIR_INS_L64="$DIR_INSTALL/lib64"
+DIR_INS_P="$DIR_INS_L/pkgconfig"
+DIR_INS_P64="$DIR_INS_L64/pkgconfig"
+DIR_INS_I="$DIR_INSTALL/include/sexp"
 
 DIR_TESTS="$( cd "$DIR0/.." && pwd)"
 
